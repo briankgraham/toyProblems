9fd5dc288363eeda9aaec9e3bac9a198dc058ac6
Brian Graham <graham.briank@gmail.com>
2016-02-11 17:21:10 -0800

    add ability to remove root and set heap for min or max

--- binaryHeap.js
+++ binaryHeap.js
@@ -1,25 +1,53 @@
-var binaryHeap = function () {
+var binaryHeap = function (max) {
   this.heap = [];
-
-}
+  this.compare = function (idx1, idx2) { // (child, parent) --> min heap
+    return max === 'max' ? this.heap[idx1] > this.heap[idx2] : this.heap[idx1] < this.heap[idx2];
+  }
+};
 
 binaryHeap.prototype.swap = function (index1, index2) {
   var temp = this.heap[index1];
   this.heap[index1] = this.heap[index2];
   this.heap[index2] = temp;
-}
+};
 
 binaryHeap.prototype.leftChild = function (index) {
-  return (2*index)+1;
+  return (2*index) + 1;
 };
 
 binaryHeap.prototype.rightChild = function (index) {
-  return (2*index)+2;
+  return (2*index) + 2;
 };
 
 binaryHeap.prototype.getParent = function (index) {
   return Math.floor((index - 1) / 2);
-}
+};
+
+binaryHeap.prototype.smallestIndex = function (idx1, idx2) {
+  return this.heap[idx1] < this.heap[idx2] ? idx1 : idx2;
+};
+
+binaryHeap.prototype.removeRoot = function () {
+
+  this.swap(0, this.heap.length - 1);
+
+  var deleted = this.heap.pop();
+
+  var parent = 0;
+
+  var children = [this.leftChild(parent), this.rightChild(parent)];
+
+  var smallChild = this.smallestIndex(children[0], children[1]);
+
+  while (this.compare(smallChild, parent)) {
+    this.swap(smallChild, parent);
+    parent = smallChild;
+    children = [this.leftChild(parent), this.rightChild(parent)];
+    smallChild = this.smallestIndex(children[0], children[1]);
+  }
+
+  return deleted;
+};
 
 binaryHeap.prototype.insert = function (value) {
   if (this.heap[0] === undefined) {
@@ -33,7 +61,7 @@ binaryHeap.prototype.insert = function (value) {
   // compare to its parent
   var parent = this.getParent(lastChild);
   // if greater than parent, swap
-  while (this.heap[lastChild] < this.heap[parent]) {
+  while (this.compare(lastChild, parent)) {
     this.swap(parent, lastChild);
     lastChild = parent;
     parent = this.getParent(lastChild);
@@ -41,11 +69,12 @@ binaryHeap.prototype.insert = function (value) {
 };
 
  // floor((i − 1) ∕ 2)
-var bHeap = new binaryHeap();
+var bHeap = new binaryHeap('max');
 bHeap.insert(1);
 bHeap.insert(5);
 bHeap.insert(3);
 
 bHeap.insert(0);
+bHeap.removeRoot();
 
 console.log(bHeap.heap);
